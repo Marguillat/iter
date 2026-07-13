8c32507eca6e6709453a8aa1ec6b4c4c816a7f51
margouillat <margouillat_ez@proton.me>
2026-07-13 22:36:25 +0200

    error handling

--- open-api/main.go
+++ open-api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"iter-api/database"
+	"iter-api/utils"
 	"log"
 
 	"github.com/gofiber/contrib/v3/monitor"
@@ -22,18 +23,12 @@ func main() {
 	app.Get("/metrics", monitor.New(monitor.Config{APIOnly: true}))
 
 	// Route de base de l'API
-	defaultMessage := fiber.Map{
-		"status":   "ok",
-		"messages": "Welcome to the open ITER dpp API. All routes start at the /api path. For more information see the swagger specification.",
-		"version":  "1.0.0",
-	}
-
 	app.Get("/", func(c fiber.Ctx) error {
-		return c.JSON(defaultMessage)
+		return c.JSON(utils.DefaultMessage)
 	})
 
 	app.Get("/api", func(c fiber.Ctx) error {
-		return c.JSON(defaultMessage)
+		return c.JSON(utils.DefaultMessage)
 	})
 
 	app.Get("/api/dpp/:gtin?", func(c fiber.Ctx) error {
@@ -42,15 +37,17 @@ func main() {
 		case c.Params("gtin") != "":
 			DB, err := database.ConnectToDB()
 			if err != nil {
-				return fmt.Errorf("Error while connecting to db: %e", err)
+				log.Printf("Error while connecting to db: %e", err)
+				return c.Status(503).JSON(utils.InternalServerError)
 			}
 			getPassportResponse, err := database.GetPassportByGTIN(DB.CurrentConn, c.Params("gtin"))
 			if err != nil {
-				return fmt.Errorf("Error while requesting passport: %e", err)
+				log.Printf("Error while requesting passport to db: %e", err)
+				return c.Status(503).JSON(utils.InternalServerError)
 			}
 			return c.JSON(getPassportResponse)
 		default:
-			return c.JSON(defaultMessage)
+			return c.Status(404).JSON(utils.NoRouteMessage)
 		}
 	})
 
--- open-api/utils/utils.go
+++ open-api/utils/utils.go
@@ -3,8 +3,30 @@ package utils
 import (
 	"fmt"
 	"regexp"
+
+	"github.com/gofiber/fiber/v3"
 )
 
+// ---- constants ----
+var DefaultMessage fiber.Map = fiber.Map{
+	"status":   "ok",
+	"messages": "Welcome to the open ITER dpp API. All routes start at the /api path. For more information see the swagger specification.",
+	"version":  "1.0.0",
+}
+
+var NoRouteMessage fiber.Map = fiber.Map{
+	"status":   "ok",
+	"messages": "Nothing on this route, might be missing route path parameter. For more information see the swagger specification.",
+	"version":  "1.0.0",
+}
+
+var InternalServerError fiber.Map = fiber.Map{
+	"status":   "ko",
+	"messages": "Internal server error, bruh",
+	"version":  "1.0.0",
+}
+
+// ---- Functions ----
 func CheckIsGTIN(s *string) (bool, error) {
 	const gtinRegexp string = "^[0-9]{8,14}$"
 	match, err := regexp.Match(gtinRegexp, []byte(*s))
